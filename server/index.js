22a83d667d7b1a01cb8e4a59b9b923caecbf978e
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-01-23 01:46:46 +0530

    .env correct

--- server/index.js
+++ server/index.js
@@ -88,7 +88,7 @@ app.post('/api/uploadImage', upload.single('file'), (req, res) => {
     }
 
     // Make sure to return an absolute URL
-    const fullUrl = `https://emailbuilderserver.onrender.com/uploads/${req.file.filename}`;
+    const fullUrl = `https://email-builder-5oko.vercel.app/uploads/${req.file.filename}`;
     // const fullUrl =const fullUrl =
     
     res.json({
